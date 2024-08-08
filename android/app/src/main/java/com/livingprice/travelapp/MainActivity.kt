d9a77326e200340eb3add1536e9700618ef7062e
umit-turk <uyt_turk@hotmail.com>
2024-08-09 02:05:49 +0300

    wip

--- android/app/src/main/java/com/livingprice/travelapp/MainActivity.kt
+++ android/app/src/main/java/com/livingprice/travelapp/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.livingprice
+package com.livingprice.travelapp
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
@@ -11,7 +11,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "LivingPrice"
+  override fun getMainComponentName(): String = "Living Price"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
--- android/app/src/main/java/com/livingprice/travelapp/MainApplication.kt
+++ android/app/src/main/java/com/livingprice/travelapp/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.livingprice
+package com.livingprice.travelapp
 
 import android.app.Application
 import com.facebook.react.PackageList
